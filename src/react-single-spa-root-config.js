8e3924c028cc8ea9145f7812c605d1bdb825e750
Nilo Neregato <nilo.neregato@scania.com>
2020-06-11 17:03:04 +0200

    trying to render mfe

--- src/react-single-spa-root-config.js
+++ src/react-single-spa-root-config.js
@@ -1,13 +1,13 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
 
 // registerApplication({
 //   name: "@react-single-spa/navbar",
